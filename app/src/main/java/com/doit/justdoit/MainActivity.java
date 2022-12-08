bab26f106ac9d519b16adac05fc1a98a5b25c931
SamsungIlnurio <nurich1995@mail.ru>
2022-12-08 16:53:36 +0300

    add Handler in Threads

--- app/src/main/java/com/doit/justdoit/MainActivity.java
+++ app/src/main/java/com/doit/justdoit/MainActivity.java
@@ -2,6 +2,8 @@ package com.doit.justdoit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton buttonAddNotes;
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private NotesAdapter notesAdapter;
 
     private NoteDatabase noteDatabase;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +60,20 @@ public class MainActivity extends AppCompatActivity {
                             @NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                         int position = viewHolder.getAdapterPosition();
                         Note note = notesAdapter.getNotes().get(position);
-                        noteDatabase.notesDao().remove(note.getId());
-                        showNotes();
+
+                        Thread thread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                noteDatabase.notesDao().remove(note.getId());
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        showNotes();
+                                    }
+                                });
+                            }
+                        });
+                        thread.start();
                     }
                 });
         itemTouchHelper.attachToRecyclerView(recyclerViewNotes);
@@ -81,6 +98,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showNotes() {
-        notesAdapter.setNotes(noteDatabase.notesDao().getNotes());
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List <Note> notes = noteDatabase.notesDao().getNotes();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        notesAdapter.setNotes(notes);
+                    }
+                });
+            }
+        });
+        thread.start();
     }
 }
